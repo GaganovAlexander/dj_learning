6541fc5d3e25efef0f0c464dea25ce6912683f53
Alexander <althgamer@althgamer.ru>
2024-02-22 17:00:05 +0300

    init

---
+++ learning/learning/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+def root(request):
+    return render(request, 'root/index.html')
+
+def ex1_12(request):
+    return render(request, 'task-3/ex1.1-12.html')
+
+def ex13(request):
+    return render(request, 'task-3/ex1.13.html')
+
+def ex14(request):
+    return render(request, 'task-3/ex1.14.html')
+
+def ex15(request):
+    return render(request, 'task-3/ex1.15.html')
